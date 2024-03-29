797680939e90154803ca56586fc7657e3924bfbe
teo-clone <theodore.tsivranidis@gmail.com>
2024-03-29 15:01:14 -0700

    fixing background bug on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import '../app.css';
-
-	let video: HTMLVideoElement;
-
-	onMount(() => {
-		if (
-			/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-		) {
-			video.removeAttribute('autoplay');
-			video.removeAttribute('loop');
-		}
-	});
 </script>
 
 <div class="flex justify-center min-h-screen">
 	<div class="flex max-w-[700px] flex-col gap-5 px-2 py-5 justify-center items-center">
-		<video bind:this={video} autoplay muted loop>
+		<video autoplay muted loop>
 			<source src="ps2_startup.mp4" type="video/mp4" />
 		</video>
 		<slot />
